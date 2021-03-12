1e6e925de7bf246a7017d51be6c24f338ed97a5d
Andy Phillipson <ajp@mindspring.com>
2021-03-12 12:08:21 -0500

    Add dist to repo while waiting for Sentry to push to npm

---
+++ dist/js/options.d.ts
@@ -0,0 +1,44 @@
+import { BrowserOptions } from "@sentry/react";
+/**
+ * Configuration options for the Sentry ReactNative SDK.
+ * @see ReactNativeFrontend for more information.
+ */
+export interface ReactNativeOptions extends BrowserOptions {
+    /**
+     * Enables native transport + device info + offline caching.
+     * Be careful, disabling this also breaks automatic release setting.
+     * This means you have to manage setting the release yourself.
+     * Defaults to `true`.
+     */
+    enableNative?: boolean;
+    /**
+     * Enables native crashHandling. This only works if `enableNative` is `true`.
+     * Defaults to `true`.
+     */
+    enableNativeCrashHandling?: boolean;
+    /**
+     * Initializes the native SDK on init.
+     * Set this to `false` if you have an existing native SDK and don't want to re-initialize.
+     *
+     * NOTE: Be careful and only use this if you know what you are doing.
+     * If you use this flag, make sure a native SDK is running before the JS Engine initializes or events might not be captured.
+     * Also, make sure the DSN on both the React Native side and the native side are the same one.
+     * We strongly recommend checking the documentation if you need to use this.
+     *
+     * @default true
+     */
+    autoInitializeNativeSdk?: boolean;
+    /** Maximum time to wait to drain the request queue, before the process is allowed to exit. */
+    shutdownTimeout?: number;
+    /** Should the native nagger alert be shown or not. */
+    enableNativeNagger?: boolean;
+    /** Should sessions be tracked to Sentry Health or not. */
+    enableAutoSessionTracking?: boolean;
+    /** The interval to end a session if the App goes to the background. */
+    sessionTrackingIntervalMillis?: number;
+    /** Enable scope sync from Java to NDK on Android */
+    enableNdkScopeSync?: boolean;
+    /** When enabled, all the threads are automatically attached to all logged events on Android */
+    attachThreads?: boolean;
+}
+//# sourceMappingURL=options.d.ts.map
